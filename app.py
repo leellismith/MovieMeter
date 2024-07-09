27c864a9232b6be47256dc6266824d6a50df0880
Lee Smith <leellismith@hotmail.co.uk>
2024-07-09 13:24:48 +0000

    Fixing issues with search/autocomplete

--- app.py
+++ app.py
@@ -3,23 +3,26 @@ import requests
 import datetime
 from flask import (
     Flask, flash, render_template,
-    redirect, request, jsonify, session, url_for)
+    redirect, request, jsonify, session, url_for
+)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
-
+# Initialize Flask
 app = Flask(__name__)
 
+# Configuration
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+# Initialize PyMongo
 mongo = PyMongo(app)
 
-
+# Routes
 @app.route("/")
 @app.route("/index")
 def index():
@@ -34,14 +37,10 @@ def search():
     movies = search_movies_in_db(query)
     
     if not movies:
-        movies = search_movies_in_api(query)
-        if movies:
-            save_movies_to_db(movies)
-        else:
-            if redirect_to == "add_review":
-                flash("No movies found.")
-                return render_template("add_review.html", movie_title=query)
-            return render_template("index.html", error="No movies found.")
+        if redirect_to == "add_review":
+            flash(f"No movies found for '{query}'.")
+            return render_template("add_review.html", movie_title=query)
+        return render_template("index.html", error=f"No movies found for '{query}'.")
         
     if redirect_to == "add_review":
         poster_url = movies[0].get("Poster") if movies else None
@@ -54,63 +53,8 @@ def search():
 @app.route("/autocomplete", methods=["GET"])
 def autocomplete():
     query = request.args.get("query")
-    movies = search_movies_in_db(query)
-    
-    if not movies:
-        movies = search_movies_in_api(query)
-        if movies:
-            save_movies_to_db(movies)
-    
+    movies = search_movies(query)    
     return jsonify([movie["Title"] for movie in movies])
-
-def search_movies_in_db(query):
-    movies = list(mongo.db.movies.find({"$text": {"$search": query}}))
-    return [{"Title": movie["Title"], "Poster": movie.get("Poster")} for movie in movies]
-
-def search_movies_in_api(query):
-    api_url = os.environ.get(
-        "OMDBAPI_HOST") + "apikey=" + os.environ.get(
-            "OMDBAPI_KEY") + "&s=" + query
-    response = requests.get(api_url)
-    
-    if response.status_code == 200:
-        search_results = response.json()
-        if 'Search' in search_results:
-            movies = []
-            for movie in search_results['Search']:
-                movie_details = get_movie_details(movie['imdbID'])
-                if movie_details:
-                    movies.append(movie_details)
-            return movies
-    return None
-
-def get_movie_details(imdb_id):
-    api_url = os.environ.get(
-        "OMDBAPI_HOST") + "apikey=" + os.environ.get(
-            "OMDBAPI_KEY") + "&i=" + imdb_id
-    response = requests.get(api_url)
-    
-    if response.status_code == 200:
-        movie_data = response.json()
-        if 'Error' not in movie_data:
-            return {
-                "Title": movie_data.get("Title"),
-                "Actors": movie_data.get("Actors"),
-                "Director": movie_data.get("Director"),
-                "Genre": movie_data.get("Genre"),
-                "Poster": movie_data.get("Poster"),
-                "Rated": movie_data.get("Rated"),
-                "Released": movie_data.get("Released"),
-                "Runtime": movie_data.get("Runtime"),
-                "Year": movie_data.get("Year"),
-                "imdbID": movie_data.get("imdbID")
-            }
-    return None
-
-def save_movies_to_db(movies):
-    for movie in movies:
-        if not mongo.db.movies.find_one({"imdbID": movie.get("imdbID")}):
-            mongo.db.movies.insert_one(movie)
     
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -142,109 +86,55 @@ def login():
         # Checks database to see if user exists
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-
-        if existing_user:
-            # ensures password matches
-            if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(
-                        request.form.get("username")))
-                    return redirect(
-                        url_for("profile", username=session["user"]))
-            else:
-                # invalid password match
-                flash("Incorrect Username and/or Password")
-                return redirect(url_for("login"))
-
-        else:
-            # username doesn't exist
-            flash("Incorrect Username and/or Password")
-            return redirect(url_for("login"))
+        if existing_user and check_password_hash(existing_user["password"], request.form.get("password")):
+            session["user"] = request.form.get("username").lower()
+            flash(f"Welcome, {request.form.get('username')}")
+            return redirect(url_for("profile", username=session["user"]))
+        flash("Incorrect Username and/or Password")
     return render_template("login.html")
 
 
-@app.route("/profile/<username>", methods=["GET", "POST"])
+@app.route("/profile/<username>")
 def profile(username):
-    # Get the session user from session
-    sess_user = session.get("user")
-
-    if sess_user:
-        # Get the session user from DB
-        user = mongo.db.users.find_one({"username": sess_user})
-        
-        if user:
-            # Check if the session user matches the username in the URL
-            if user["username"] == username:
-                return render_template("profile.html", username=user["username"])
-            else:
-                flash("You are only able to view your own profile!")
-                return redirect(url_for("profile", username=user["username"]))
-        else:
-            flash("Can't find user in the database.")
-            return redirect(url_for("login"))
-    else:
-        return redirect(url_for("login"))
+    if "user" in session:
+        user = mongo.db.users.find_one({"username": session["user"]})
+        if user and user["username"] == username:
+            return render_template("profile.html", username=username)
+        flash("You are only able to view your own profile!")
+        return redirect(url_for("profile", username=session["user"]))
+    return redirect(url_for("login"))
 
 
 @app.route("/logout")
 def logout():
     flash("You have been logged out")
-    session.pop("user")
+    session.pop("user", None)
     return redirect(url_for("login"))
 
 
 @app.route("/add_review", methods=["GET", "POST"])
 def add_review():
-    movie_title = ""
-    review_text = ""
-    poster_url = ""
+    movie_title = request.form.get("query")
+    review_text = request.form.get("review")
+    poster_url = request.form.get("poster_url")
 
-    if request.method == "POST":
-        movie_title = request.form.get("query")
-        review_text = request.form.get("review")
-        poster_url = request.form.get("poster_url")
-        
-        if not movie_title or not review_text:
-            return render_template(
-                "add_review.html", movie_title=movie_title, 
-                review_text=review_text, poster_url=poster_url)
-
+    if request.method == "POST" and movie_title and review_text:
         if len(review_text) > 200:
             flash("A review must be 200 characters or less")
-            return render_template(
-                "add_review.html", movie_title=movie_title, 
-                review_text=review_text, poster_url=poster_url)
-
-        movie_details = None
-        movies_db = search_movies_in_db(movie_title)
-        if movies_db:
-            movie_details = movies_db[0]  # Takes the first result from DB
-
-        if not movie_details:
-            movies_api = search_movies_in_api(movie_title)
-            if movies_api:
-                movie_details = movies_api[0]  # Takes the first result from API
-                save_movies_to_db(movies_api)  # Saves fetched movies to DB if not already present
-
-        if not movie_details:
+        else:
+            movie_details = search_movie_details(movie_title)
+            if movie_details:
+                review = {
+                    "movie": movie_title,
+                    "review": review_text,
+                    "image_url": poster_url,
+                    "user": session["user"],
+                    "timestamp": datetime.datetime.utcnow()
+                }
+                mongo.db.reviews.insert_one(review)
+                flash("Review added successfully.")
+                return redirect(url_for("reviews"))
             flash(f"Movie '{movie_title}' not found.")
-            return render_template(
-                "add_review.html", movie_title=movie_title, review_text=review_text,
-                    poster_url=poster_url)
-
-        review = {
-            "movie": movie_title,
-            "review": review_text,
-            "image_url": poster_url,
-            "user": session["user"],
-            "timestamp": datetime.datetime.utcnow()
-        }
-
-        # Insert review into MongoDB
-        mongo.db.reviews.insert_one(review)
-        flash("Review added successfully.")
-        return redirect(url_for("reviews"))  # Redirect to reviews page
 
     return render_template("add_review.html", movie_title=movie_title, review_text=review_text, poster_url=poster_url)
 
@@ -255,7 +145,69 @@ def reviews():
     return render_template("reviews.html", reviews=reviews)
 
 
+# Help Functions
+def search_movies(query):
+    movies = search_movies_in_db(query)
+    if not movies:
+        movies = search_movies_in_api(query)
+        if movies:
+            save_movies_to_db(movies)
+    return movies
+
+
+def search_movies_in_db(query):
+    return list(mongo.db.movies.find({"$text": {"$search": query}}))
+
+
+def search_movies_in_api(query):
+    api_url = os.environ.get(
+        "OMDBAPI_HOST") + "apikey=" + os.environ.get(
+            "OMDBAPI_KEY") + "&s=" + query
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        search_results = response.json()
+        if 'Search' in search_results:
+            movies = []
+            for movie in search_results['Search']:
+                movie_details = get_movie_details(movie['imdbID'])
+                if movie_details:
+                    movies.append(movie_details)
+            return movies
+    return None
+
+
+def get_movie_details(imdb_id):
+    api_url = os.environ.get(
+        "OMDBAPI_HOST") + "apikey=" + os.environ.get(
+            "OMDBAPI_KEY") + "&i=" + imdb_id
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        movie_data = response.json()
+        if 'Error' not in movie_data:
+            return {
+                "Title": movie_data.get("Title"),
+                "Actors": movie_data.get("Actors"),
+                "Director": movie_data.get("Director"),
+                "Genre": movie_data.get("Genre"),
+                "Poster": movie_data.get("Poster"),
+                "Rated": movie_data.get("Rated"),
+                "Released": movie_data.get("Released"),
+                "Runtime": movie_data.get("Runtime"),
+                "Year": movie_data.get("Year"),
+                "imdbID": movie_data.get("imdbID")
+            }
+    return None
+
+
+def save_movies_to_db(movies):
+    for movie in movies:
+        if not mongo.db.movies.find_one({"imdbID": movie.get("imdbID")}):
+            mongo.db.movies.insert_one(movie)
+
+
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=True
+    )
--- static/js/script.js
+++ static/js/script.js
@@ -1,52 +1,43 @@
-document.getElementById('query').addEventListener('input', function() {
-  let query = this.value.trim();
-  if (query.length < 2) {
-      document.getElementById('autocomplete-list').innerHTML = '';
-      return;
-  }
-  
-  fetch(`/autocomplete?query=${query}`)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          // Additional logging to capture the response before parsing
-          return response.text().then(text => {
-              try {
-                  return JSON.parse(text);
-              } catch (err) {
-                  console.error('Response is not valid JSON:', text);
-                  throw new Error('Response is not valid JSON');
-              }
-          });
-      })
-      .then(data => {
-          let suggestions = data.map(title => `<div class="autocomplete-suggestion">${title}</div>`).join('');
-          document.getElementById('autocomplete-list').innerHTML = suggestions;
+document.addEventListener('DOMContentLoaded', function () {
+    const queryInput = document.getElementById('query');
+    const autocompleteList = document.getElementById('autocomplete-list');
 
-          document.querySelectorAll('.autocomplete-suggestion').forEach(item => {
-              item.addEventListener('click', function () {
-                  document.getElementById('query').value = this.innerText;
-                  document.getElementById('autocomplete-list').innerHTML = '';
-              });
-          });
-      })
-      .catch(error => {
-          console.error('Error fetching autocomplete suggestions:', error);
-          document.getElementById('autocomplete-list').innerHTML = '<div class="autocomplete-error">Unable to find the movie</div>';
-      });
-  });
+    queryInput.addEventListener('input', function() {
+        let query = this.value.trim();
+        if (query.length < 2) {
+            autocompleteList.innerHTML = '';
+            return;
+        }
 
-// Close autocomplete suggestions when clicking outside
-document.addEventListener('click', function(e) {
-    if (!e.target.classList.contains('autocomplete-suggestion') && e.target.id !== 'query') {
-        document.getElementById('autocomplete-list').innerHTML = '';
-    }
+        fetch(`/autocomplete?query=${encodeURIComponent(query)}`)  // Ensures proper URL encoding
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();  // Parse response as JSON directly
+            })
+            .then(data => {
+                let suggestions = data.map(title => `<div class="autocomplete-suggestion">${title}</div>`).join('');
+                autocompleteList.innerHTML = suggestions;
+
+                document.querySelectorAll('.autocomplete-suggestion').forEach(item => {
+                    item.addEventListener('click', function () {
+                        queryInput.value = this.innerText;
+                        autocompleteList.innerHTML = '';
+                    });
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching autocomplete suggestions:', error);
+                autocompleteList.innerHTML = '<div class="autocomplete-error">Unable to find the movie</div>';
+            });
+    });
+
+    // Close autocomplete suggestions when clicking outside
+    document.addEventListener('click', function(e) {
+        if (!e.target.classList.contains('autocomplete-suggestion') && e.target.id !== 'query') {
+            autocompleteList.innerHTML = '';
+        }
+    });
+
 });
-
-// jQuery code safely wrapped in noConflict mode
-jQuery(document).ready(function($) {
-  // Use $ safely within this function
-  $('.sidenav').sidenav();
-});
-
